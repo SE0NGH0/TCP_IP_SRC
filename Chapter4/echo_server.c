bbebe331da5092098410ea5dd1cb2f961bd797c0
Seongho <140130211+SE0NGH0@users.noreply.github.com>
2025-05-20 16:06:51 +0900

    Add files via upload

---
+++ Chapter4/echo_server.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define BUF_SIZE 1024
+void error_handling(char *message);
+
+void* thread_main(void *arg);
+
+int main(int argc, char *argv[])
+{
+	pthread_t t_id[5];
+
+	int serv_sock, clnt_sock;
+	char message[BUF_SIZE];
+	int str_len, i;
+	
+	struct sockaddr_in serv_adr;
+	struct sockaddr_in clnt_adr;
+	socklen_t clnt_adr_sz;
+	
+	if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);   
+	if(serv_sock==-1)
+		error_handling("socket() error");
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port=htons(atoi(argv[1]));
+
+	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+		error_handling("bind() error");
+	
+	if(listen(serv_sock, 5)==-1)
+		error_handling("listen() error");
+	
+	clnt_adr_sz=sizeof(clnt_adr);
+
+	for(i=0; i<5; i++)
+	{
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+		if(clnt_sock==-1)
+			error_handling("accept() error");
+		else
+			printf("Connected client %d \n", i+1);
+	
+		pthread_create(&(t_id[i]), NULL, thread_main, (void*)&clnt_sock);
+		pthread_detach(t_id[i]);	// 논블럭킹
+	}
+
+	close(serv_sock);
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+void* thread_main(void *arg) 
+{
+	int sock=*((int*)arg);
+	int str_len;
+	char message[BUF_SIZE];
+
+	while((str_len=read(sock, message, BUF_SIZE)) != 0){
+			write(sock, message, str_len);
+	}
+
+	close(sock);
+	
+	return NULL;
+}
